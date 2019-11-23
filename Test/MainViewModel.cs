bf87922a8609d6f049e8182fd965421f7cef48c5
miswil <natura-facit-saltus1900via_planck@outlook.jp>
2019-11-24 02:07:38 +0900

    :add: initial implementation of listview.
    1.select behavior of windows explorer
    2.select by drag

---
+++ Test/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Product> Products { get; }
+
+        public MainViewModel()
+        {
+            this.Products = new ObservableCollection<Product>(
+                Enumerable.Range(0, 100).Select(i => new Product { Name = $"Product{i}", Weight = i }));
+        }
+    }
+}
