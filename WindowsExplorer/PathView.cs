b9d503211fdb83d612eda4465fd21dcb79d21ec2
miswil <natura-facit-saltus1900via_planck@outlook.jp>
2019-12-10 22:04:30 +0900

    :add: implement the edit mode functionality.
    clicking empty area invokes the edit mode. edit the text and commit with 'Enter' or cancel with 'Esc'.

--- WindowsExplorer/PathView.cs
+++ WindowsExplorer/PathView.cs
@@ -37,6 +37,8 @@ namespace WindowsExplorer
     public class PathView : ItemsControl
     {
         private PathViewItem expandingItem = null;
+        private TextBox pathTextBox = null;
+        private string beforeEditPathText;
 
         #region Property
         public PathViewState State
@@ -48,6 +50,16 @@ namespace WindowsExplorer
         // Using a DependencyProperty as the backing store for Status.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty StateProperty =
             DependencyProperty.Register("State", typeof(PathViewState), typeof(PathView), new PropertyMetadata(PathViewState.Normal));
+
+        public string PathText
+        {
+            get { return (string)GetValue(PathTextProperty); }
+            set { SetValue(PathTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Path.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PathTextProperty =
+            DependencyProperty.Register("PathText", typeof(string), typeof(PathView), new PropertyMetadata(null, PathTextPropertyChanged));
         #endregion Property
 
         #region Events
@@ -62,6 +74,18 @@ namespace WindowsExplorer
             add { AddHandler(SelectedEvent, value); }
             remove { RemoveHandler(SelectedEvent, value); }
         }
+
+        public delegate void PathTextChangedEventHandler(object sender, PathTextChangedEventArgs e);
+
+        public static readonly RoutedEvent PathTextChangedEvent =
+            EventManager.RegisterRoutedEvent("PathTextChanged", RoutingStrategy.Bubble, typeof(PathTextChangedEventHandler), typeof(PathView));
+
+        // Provide CLR accessors for the event
+        public event PathTextChangedEventHandler PathTextChanged
+        {
+            add { AddHandler(PathTextChangedEvent, value); }
+            remove { RemoveHandler(PathTextChangedEvent, value); }
+        }
         #endregion Events
 
         static PathView()
@@ -69,11 +93,40 @@ namespace WindowsExplorer
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PathView), new FrameworkPropertyMetadata(typeof(PathView)));
         }
 
+        private static void PathTextPropertyChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((sender as PathView).pathTextBox != null)
+            {
+                (sender as PathView).pathTextBox.Text = (string)e.NewValue;
+            }
+        }
+
         public PathView()
         {
             this.AddHandler(PathViewItemChildItem.SelectedEvent, new RoutedEventHandler(this.ItemSelected));
             this.AddHandler(PathViewItem.SelectedEvent, new RoutedEventHandler(this.ItemSelected));
-            this.PreviewMouseDown += this.PathView_PreviewMouseDown;
+            this.AddHandler(Mouse.PreviewMouseDownOutsideCapturedElementEvent, (MouseButtonEventHandler)this.PathView_PreviewMouseDownOutside, true);
+            this.MouseUp += this.PathView_MouseUp;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (this.pathTextBox != null)
+            {
+                this.pathTextBox.IsVisibleChanged -= this.PathTextBox_IsVisibleChanged;
+                this.pathTextBox.LostMouseCapture -= this.PathTextBox_LostMouseCapture;
+                this.pathTextBox.KeyDown -= this.PathTextBox_KeyDown;
+            }
+            this.pathTextBox = this.Template.FindName("PART_PathText", this) as TextBox;
+            if (this.pathTextBox != null)
+            {
+                this.pathTextBox.Text = this.PathText;
+                this.pathTextBox.KeyDown += this.PathTextBox_KeyDown;
+                this.pathTextBox.LostMouseCapture += this.PathTextBox_LostMouseCapture;
+                this.pathTextBox.IsVisibleChanged += this.PathTextBox_IsVisibleChanged;
+            }
         }
 
         protected override DependencyObject GetContainerForItemOverride()
@@ -161,15 +214,96 @@ namespace WindowsExplorer
             this.ReleaseMouseCapture();
         }
 
-        private void PathView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        /// <summary>
+        /// back the state to normal when user cancel to input the pathtext.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PathView_PreviewMouseDownOutside(object sender, MouseButtonEventArgs e)
         {
             if (this.State == PathViewState.Expanding)
             {
-                if(this.IsMouseDirectlyOver)
-                {
-                    this.ReleaseMouseCapture();
-                    this.expandingItem.IsExpanded = false;
-                }
+                this.ReleaseMouseCapture();
+                this.expandingItem.IsExpanded = false;
+            }
+            this.pathTextBox.Text = this.beforeEditPathText;
+            this.SetVisualState(PathViewState.Normal);
+        }
+
+        /// <summary>
+        /// force mouse captured because textbox default behavior release the mouse capture when selecting the text in textbox with mouse drag/click.
+        /// But when user is opening a contextmenu, although mouse capture is lost from the textbox, not force the textbox to capture mouse to prevent showing context menu.
+        /// In order to get contextmenu IsOpen, ContextMenu is explicitly defined for the textbox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PathTextBox_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (!this.pathTextBox.ContextMenu.IsOpen)
+            {
+                Mouse.Capture(this.pathTextBox, CaptureMode.SubTree);
+            }
+        }
+
+        /// <summary>
+        /// TextBox focus and select must be done after it is visualized, not at visibility is set.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PathTextBox_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                this.pathTextBox.Focus();
+                this.pathTextBox.SelectAll();
+                Mouse.Capture(this.pathTextBox, CaptureMode.SubTree);
+            }
+        }
+
+        private void PathView_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (this.expandingItem != null)
+            {
+                this.expandingItem.IsExpanded = false;
+            }
+            this.beforeEditPathText = this.pathTextBox.Text;
+            this.SetVisualState(PathViewState.Editing);
+        }
+
+        private void PathTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    this.SetVisualState(PathViewState.Normal);
+                    this.PathText = this.pathTextBox.Text;
+                    this.RaiseEvent(new PathTextChangedEventArgs(PathTextChangedEvent) { OldText = this.beforeEditPathText, NewText = this.pathTextBox.Text });
+                    break;
+                case Key.Escape:
+                    this.SetVisualState(PathViewState.Normal);
+                    this.pathTextBox.Text = this.beforeEditPathText;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SetVisualState(PathViewState state)
+        {
+            switch (state)
+            {
+                case PathViewState.Normal:
+                    VisualStateManager.GoToState(this, "Normal", false);
+                    this.State = PathViewState.Normal;
+                    break;
+                case PathViewState.Expanding:
+                    break;
+                case PathViewState.Editing:
+                    VisualStateManager.GoToState(this, "Edit", false);
+                    this.State = PathViewState.Editing;
+                    break;
+                default:
+                    break;
             }
         }
     }
@@ -192,5 +326,26 @@ namespace WindowsExplorer
         }
 
         public object Item { get; internal set; }
+    }
+
+    public class PathTextChangedEventArgs : RoutedEventArgs
+    {
+        public PathTextChangedEventArgs() : base()
+        {
+
+        }
+
+        public PathTextChangedEventArgs(RoutedEvent routedEvent) : base(routedEvent)
+        {
+
+        }
+
+        public PathTextChangedEventArgs(RoutedEvent routedEvent, object source) : base(routedEvent, source)
+        {
+
+        }
+
+        public string OldText { get; internal set; }
+        public string NewText { get; internal set; }
     }
 }
