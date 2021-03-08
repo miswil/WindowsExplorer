ba5d0e8d33aeebbb372feeddf299bf5563717fae
miswil <natura-facit-saltus1900via_planck@outlook.jp>
2021-03-08 21:59:00 +0900

    :add: add TreeListView Control

---
+++ WindowsExplorer/TreeListViewItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeisterWill.WindowsExplorer
+{
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WindowsExplorer.Themes"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WindowsExplorer.Themes;assembly=WindowsExplorer.Themes"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:TreeListViewItem/>
+    ///
+    /// </summary>
+    public class TreeListViewItem : TreeViewItem
+    {
+        public int Depth
+        {
+            get { return (int)GetValue(DepthProperty); }
+            set { SetValue(DepthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Depth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DepthProperty =
+            DependencyProperty.Register("Depth", typeof(int), typeof(TreeListViewItem), new PropertyMetadata(0));
+
+        static TreeListViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeListViewItem), new FrameworkPropertyMetadata(typeof(TreeListViewItem)));
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new TreeListViewItem
+            {
+                Depth = this.Depth + 1
+            };
+        }
+    }
+}
